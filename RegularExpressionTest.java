293fd50042183a586f799105155c445573906aa7
Przemyslaw Waberski <przemekwaberski@gmai.l.com>
2018-10-20 13:13:52 +0200

    All three excersises done

--- RegularExpressionTest.java
+++ RegularExpressionTest.java
@@ -1,6 +1,7 @@
 package pl.kurs.regularne;
 
 import org.junit.jupiter.api.Test;
+import sun.security.krb5.internal.PAData;
 
 import java.util.regex.Pattern;
 
@@ -8,18 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RegularExpressionTest {
-
-    @Test
-    public void testClassWithMultipleRanges() {
-        Pattern pattern = Pattern.compile("[a-cA-C0-3]bum");
-        assertTrue(pattern.matcher("abum").matches());
-        assertTrue(pattern.matcher("Bbum").matches());
-        assertTrue(pattern.matcher("0bum").matches());
-        assertFalse(pattern.matcher("dbum").matches());
-        assertFalse(pattern.matcher("aA0bum").matches());
-        assertFalse(pattern.matcher("aB1bum").matches());
-        assertFalse(pattern.matcher("abbum").matches());
-    }
 
     @Test
     public void testFloatNumberforCorrectness()
@@ -33,5 +22,29 @@ public class RegularExpressionTest {
         assertFalse(pattern.matcher("-----123").matches());
         assertFalse(pattern.matcher("123,123,123").matches());
 
+    }
+
+    @Test
+    public void testProperHouseNumber()
+    {
+        Pattern pattern = Pattern.compile("\\d+\\D?\\\\\\d+\\D?");
+        assertTrue(pattern.matcher("123\\2A").matches());
+        assertTrue(pattern.matcher("24B\\3").matches());
+        assertTrue(pattern.matcher("12\\5").matches());
+        assertFalse(pattern.matcher("abc\\cba").matches());
+        assertFalse(pattern.matcher("44\\c").matches());
+        assertFalse(pattern.matcher("44\\c4").matches());
+        assertFalse(pattern.matcher("12").matches());
+    }
+
+    @Test
+    public void testProperCityName()
+    {
+        Pattern pattern = Pattern.compile("[A-Z][a-z]+(( |-)[A-Z][a-z]+)?");
+        assertTrue(pattern.matcher("Wroclaw").matches());
+        assertTrue(pattern.matcher("Zielona Gora").matches());
+        assertTrue(pattern.matcher("Bielsko-Biala").matches());
+        assertFalse(pattern.matcher("Ptysiow123").matches());
+        assertFalse(pattern.matcher("wroclaw").matches());
     }
 }
